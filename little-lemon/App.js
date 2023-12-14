3cd0e4b018761bb3802d4812bfffaf9e281ee70d
heejung1180 <heejung1180@outlook.com>
2023-12-14 00:14:36 -0800

    onboard next button enabled when details are entered

--- little-lemon/App.js
+++ little-lemon/App.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SQLite from 'expo-sqlite';
 import Onboarding from './screens/Onboarding'
 import HomeScreen from './screens/HomeScreen'
 
